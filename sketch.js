cc21b163afa865d63234f34405206b5b25793651
PRO-Coding <80043809+pro-whitehatjr@users.noreply.github.com>
2022-02-16 00:17:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,13 +15,20 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   
+ options={
+ isStatic:true
+ }
+ 
+ ground= bodies.rectangle(0,height-1, width*2,1,options);
+ World.add(world,ground);
  
 }
 
 function draw() {
   background(189);
- 
   Engine.update(engine);
+ 
+ rect(ground.position.x, ground.position.y,width*2,1);
   
    
 }
